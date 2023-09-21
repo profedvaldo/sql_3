86d828a0c033c377cbb3d421481f75333f8c2685
Prof.: Edvaldo Rodrigues <86244745+profedvaldo@users.noreply.github.com>
2023-09-21 17:07:57 -0300

    update da aula de 21/09/2023
    atualização do arquivo.

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ com o banco
 de dados*/
 $servername = "localhost";
 //você deu nome ao banco de dados
-$database = "bancodo2c";
+$database = "";
 $username = "root";
 $password = "";
 
@@ -17,7 +17,29 @@ $conexao = mysqli_connect(
 if (!$conexao){
     die("Falha na conexão".mysqli_connect_error());
 }
-echo "conectado com sucesso";
+//echo "conectado com sucesso";
 
+$id = $_POST["id"];
+$nome = $_POST["nome"];
+$cpf = $_POST["cpf"];
+$botao = $_POST["botao"];
+
+echo $id." ".$nome." ".$cpf." ".$botao;
+
+$sql_cadastrar = "INSERT INTO funcionarios (id, nome, cpf) VALUES('','$nome', '$cpf')";
 
 ?>
+<html>
+    <body>
+    <form name = "func" method = "post" >
+        <label>ID</label>
+        <input type ="text" name = "id" /><br />
+        <label>Nome</label>
+        <input type ="text" name = "nome" /><br />
+        <label>CPF</label>
+        <input type ="text" name = "cpf" /><br />
+        <input type ="submit" name = "botao" value = "cadastrar" />
+        <input type ="reset" name = "botao" value = "cancelar" />
+    </form>
+    </body>
+</html>
