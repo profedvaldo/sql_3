13b043c1a3ff299b4ce8bc7bdf02bd1863b6ce60
Prof.: Edvaldo Rodrigues <86244745+profedvaldo@users.noreply.github.com>
2023-09-28 14:38:11 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ com o banco
 de dados*/
 $servername = "localhost";
 //você deu nome ao banco de dados
-$database = ""; //func2c ou func2d
+$database = "func2c"; //func2c ou func2d
 $username = "root";
 $password = "";
 
@@ -24,10 +24,26 @@ $nome = $_POST["nome"];
 $cpf = $_POST["cpf"];
 $botao = $_POST["botao"];
 
-echo $id." ".$nome." ".$cpf." ".$botao;
+if(empty($botao)){
 
-$sql_cadastrar = "INSERT INTO funcionarios 
+}else if($botao == "Cadastrar"){
+    $sql = "INSERT INTO funcionarios 
     (id, nome, cpf) VALUES('','$nome', '$cpf')";
+}
+
+//aqui vou tratar erros nas operações C.E.R.A
+if(!empty($sql)){
+    if(mysqli_query($conexao, $sql)){
+        echo "Operação realizada com sucesso";
+    }else{
+        echo "Houve um erro na operação: <br />";
+        echo  mysqli_error($conexao);
+    }
+}
+
+//echo $id." ".$nome." ".$cpf." ".$botao;
+
+
 
 ?>
 <html>
@@ -39,7 +55,7 @@ $sql_cadastrar = "INSERT INTO funcionarios
         <input type ="text" name = "nome" /><br />
         <label>CPF</label>
         <input type ="text" name = "cpf" /><br />
-        <input type ="submit" name = "botao" value = "cadastrar" />
+        <input type ="submit" name = "botao" value = "Cadastrar" />
         <input type ="reset" name = "botao" value = "cancelar" />
     </form>
     </body>
